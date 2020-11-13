73dbdad01971282ba972c9d54019c7179523769a
dai-yu <1435480344@qq.com>
2020-11-13 11:16:58 +0800

    finish practice2

--- src/practice2/Student.js
+++ src/practice2/Student.js
@@ -1,1 +1,12 @@
-export class Student {}
+export class Student {
+  constructor(name, className) {
+    this.name = name;
+    this.className = className;
+    this.study = this.study.bind(this);
+  }
+
+  study() {
+    console.log(`${this.name} is moving`);
+    console.log(`${this.name} is studying in ${this.className}`);
+  }
+}
