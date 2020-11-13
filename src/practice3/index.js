5a234fe4e333badde7f0eee08def9173392ab4ec
dai-yu <1435480344@qq.com>
2020-11-13 11:42:47 +0800

    finish practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,8 @@
-export const getPoetry
+// eslint-disable-next-line import/prefer-default-export
+export const getPoetry = () =>
+  fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const { origin, author, content } = data;
+      return [origin, author, content];
+    });
